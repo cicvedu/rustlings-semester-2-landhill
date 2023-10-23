0f18e88299e646fdd9707018d8cbdbab501b9d5f
leling <34684833+landhill@users.noreply.github.com>
2023-10-23 13:32:13 +0000

    update

--- exercises/tests/tests1.rs
+++ exercises/tests/tests1.rs
@@ -10,7 +10,6 @@
 // Execute `rustlings hint tests1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[cfg(test)]
 mod tests {
--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -5,7 +5,7 @@
 
 
 fn main() {
-    let x;
+    let x = 10;
     if x == 10 {
         println!("x is ten!");
     } else {
